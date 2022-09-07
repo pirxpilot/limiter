cdb2a222ff72a82904fa6631ec58c63d5e0462b7
Damian Krzeminski <pirxpilot@furkot.com>
2022-09-07 09:06:35 +0200

    add support for promised based API

--- test/limiter.js
+++ test/limiter.js
@@ -1,8 +1,7 @@
 const limiter = require('../index.js');
 const assert = require('assert');
 
-
-describe('limiter', function () {
+describe('limiter functions', function () {
   it('should call fn at most once per interval', function (done) {
     const interval = 35;
     const l = limiter(interval);
@@ -76,7 +75,7 @@ describe('limiter', function () {
     const l = limiter(interval);
     const time = Date.now();
 
-    l.trigger(function () {});
+    l.trigger(function () { });
     l.trigger(function () {
       const delay = Date.now() - time;
       assert.ok(delay >= interval);
@@ -131,5 +130,121 @@ describe('limiter', function () {
       done();
     });
   });
+});
 
+describe('limiter promises', function () {
+  it('should call fn at most once per interval', async function () {
+    const interval = 35;
+    const l = limiter(interval);
+    const results = [];
+
+    function push() {
+      results.push(Date.now());
+    }
+
+    l.trigger(push);
+    l.trigger(push);
+    l.trigger(push);
+    await l.trigger(push);
+
+    assert.equal(results.length, 4);
+    results
+      .map(function (item, i) {
+        const prev = (i === 0) ? results[0] : results[i - 1];
+        return item - prev;
+      })
+      .forEach(function (item, i) {
+        if (i > 0) {
+          assert.ok(item >= interval);
+        }
+      });
+  });
+
+  it('should delay calls on penalty', async function () {
+    const interval = 35;
+    const penaltyInterval = 150;
+    const l = limiter(interval, penaltyInterval);
+    const results = [];
+
+    function push() {
+      results.push(Date.now());
+      if (results.length === 2) {
+        l.penalty();
+      }
+    }
+
+    l.trigger(push);
+    l.trigger(push);
+    l.trigger(push);
+    await l.trigger(push);
+
+    assert.equal(results.length, 4);
+    results
+      .map(function (item, i) {
+        const prev = (i === 0) ? results[0] : results[i - 1];
+        return item - prev;
+      })
+      .forEach(function (item, i) {
+        if (i === 0) {
+          return;
+        }
+        if (i === 2) {
+          assert.ok(item >= penaltyInterval);
+        } else {
+          assert.ok(item >= interval);
+          assert.ok(item < penaltyInterval);
+        }
+      });
+  });
+
+  it('delay works even when queue is empty', async function () {
+    const interval = 50;
+    const l = limiter(interval);
+    const time = Date.now();
+
+    l.trigger();
+    await l.trigger();
+
+    const delay = Date.now() - time;
+    assert.ok(delay >= interval);
+  });
+
+  it('penalty works even when queue is empty', async function () {
+    const interval = 50;
+    const l = limiter(interval);
+    const time = Date.now();
+
+    l.trigger(() => l.penalty());
+    await l.trigger();
+    const delay = Date.now() - time;
+    assert.ok(delay >= 5 * interval);
+  });
+
+  it('should skip interval on true', async function () {
+    const interval = 35;
+    const l = limiter(interval);
+    const results = [];
+
+    l.trigger(() => results.push(Date.now()));
+    l.trigger(push);
+    l.trigger(push);
+    await l.trigger(push);
+
+    function push() {
+      results.push(Date.now());
+      l.skip();
+    }
+
+    assert.equal(results.length, 4);
+    results
+      .map(function (item, i) {
+        const prev = (i === 0) ? results[0] : results[i - 1];
+        return item - prev;
+      })
+      .forEach(function (item, i) {
+        if (i > 1) {
+          assert.ok(item < interval);
+        }
+      });
+  });
 });
